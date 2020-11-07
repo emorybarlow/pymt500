78a6e3a722cbf16d324ec21770b820962835d067
Emory Barlow <emory.barlow@gmail.com>
2020-11-06 19:17:22 -0500

    fix purge script

--- mt500-playbook/files/iflows/Desktop/pymt500/purge_queues.py
+++ mt500-playbook/files/iflows/Desktop/pymt500/purge_queues.py
@@ -6,6 +6,11 @@ if len(sys.argv) > 1:
     queue = sys.argv[1]
 
 rabbit_conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+rabbit_conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+rabbit_channel = rabbit_conn.channel()
+rabbit_channel.queue_declare(queue='data')
+rabbit_channel.queue_declare(queue='command')
+
 cnt = 0
 method_frame, header_frame, body = rabbit_channel.basic_get(queue)
 while body:
@@ -13,4 +18,5 @@ while body:
     print cnt
     rabbit_channel.basic_ack(method_frame.delivery_tag) 
     method_frame, header_frame, body = rabbit_channel.basic_get(queue)
+
 rabbit_conn.close()
