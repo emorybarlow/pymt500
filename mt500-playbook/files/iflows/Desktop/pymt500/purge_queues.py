15c53c7325a454e3a5eb4b99a8ee0e10785de530
Emory Barlow <emory.barlow@gmail.com>
2020-11-06 19:21:35 -0500

    make purge script just reset the queues

--- mt500-playbook/files/iflows/Desktop/pymt500/purge_queues.py
+++ mt500-playbook/files/iflows/Desktop/pymt500/purge_queues.py
@@ -1,22 +1,14 @@
 import pika
 import sys
 
-queue = 'data'
-if len(sys.argv) > 1:
-    queue = sys.argv[1]
-
 rabbit_conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 rabbit_conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 rabbit_channel = rabbit_conn.channel()
-rabbit_channel.queue_declare(queue='data')
-rabbit_channel.queue_declare(queue='command')
 
-cnt = 0
-method_frame, header_frame, body = rabbit_channel.basic_get(queue)
-while body:
-    cnt += 1
-    print cnt
-    rabbit_channel.basic_ack(method_frame.delivery_tag) 
-    method_frame, header_frame, body = rabbit_channel.basic_get(queue)
+rabbit_channel.queue_delete(queue='data')
+rabbit_channel.queue_delete(queue='command')
+
+rabbit_channel.queue_declare(queue='data', arguments={'x-max-length': 10})
+rabbit_channel.queue_declare(queue='command', arguments={'x-max-length': 10})
 
 rabbit_conn.close()
